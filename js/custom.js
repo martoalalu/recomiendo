af8c5d70a6ce70967f502afcb9b785c098d45e1f
Tomás Crom <tomi.crom@gmail.com>
2023-05-31 09:19:24 -0300

    Filtro y grilla arreglada

--- js/custom.js
+++ js/custom.js
@@ -598,9 +598,98 @@ var owlSingleSlider = function () {
 
 })
 
+// Agregado por chat gpt
+
+ var siteIstotope = function() {
+  var $container = $('#posts').isotope({
+    itemSelector: '.item',
+    isFitWidth: true
+  });
+
+  $(window).resize(function(){
+    $container.isotope({
+      columnWidth: '.col-sm-3'
+    });
+  });
+
+  $container.isotope({ filter: '*' });
+
+  $('#filters').on('click', 'a', function(e) {
+    e.preventDefault();
+    var filterValue = $(this).attr('data-filter');
+    $container.isotope({ filter: filterValue });
+    $(this).toggleClass('active');
+  });
+
+  $container.imagesLoaded().progress(function() {
+    $container.isotope('layout');
+  }).done(function() {
+    $('.gsap-reveal-img').each(function() {
+      var html = $(this).html();
+      $(this).html('<div class="reveal-wrap"><span class="cover"></span><div class="reveal-content">' + html + '</div></div>');
+    });
+
+    var controller = new ScrollMagic.Controller();
+
+    var revealImg = $('.gsap-reveal-img');
+
+    if (revealImg.length) {
+      var i = 0;
+      revealImg.each(function() {
+
+        var cover = $(this).find('.cover'),
+          revealContent = $(this).find('.reveal-content'),
+          img = $(this).find('.reveal-content img');
 
 
+        var tl2 = new TimelineMax();
+        setTimeout(function() {
+          tl2
+            .tl2.set(img, { scale: '1.2', autoAlpha: 1, })
+            .to(cover, 1, {
+              marginLeft: '0', ease: Expo.easeInOut, onComplete() {
+                tl2.set(revealContent, { autoAlpha: 1 });
+                tl2.to(cover, 1, { marginLeft: '102%', ease: Expo.easeInOut });
+                tl2.to(img, 2, { scale: '1.0', ease: Linear.easeNone }, '-=2.5');
+              }
+            });
+
+        }, i * 200);
 
 
+        var scene = new ScrollMagic.Scene({
+            triggerElement: this,
+            duration: "0%",
+            reverse: false,
+            offset: "-300%",
+          })
+          .setTween(tl2)
+          .addTo(controller);
 
+        i++;
+      });
+    }
+  });
 
+  $('.js-filter').on('click', function(e) {
+    e.preventDefault();
+    $('#filters').toggleClass('active');
+  });
+
+  // Custom filter function
+  $('.filter-btn').on('click', function(e) {
+    e.preventDefault();
+    $(this).toggleClass('active');
+
+    var activeFilters = [];
+    $('.filter-btn.active').each(function() {
+      var filterValue = $(this).data('filter');
+      activeFilters.push(filterValue);
+    });
+
+    var filterString = activeFilters.join(', ');
+    $container.isotope({ filter: filterString });
+  });
+
+}
+siteIstotope();
